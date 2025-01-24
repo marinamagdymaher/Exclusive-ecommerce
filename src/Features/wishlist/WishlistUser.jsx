5e5b567113c8485eb56b5ac69edde5ba1571e767
Marina Magdy <marinamagdymaher59@gmail.com>
2025-01-24 03:19:12 +0200

    git commit -m "Your Wishlist is now empty."

--- src/Features/wishlist/WishlistUser.jsx
+++ src/Features/wishlist/WishlistUser.jsx
@@ -3,15 +3,26 @@ import Card from "../products/Card";
 
 export default function WishlistUser() {
   const { listWishlist } = useCart();
-  console.log(listWishlist)
+  console.log(listWishlist);
   return (
-    <div>
-      <h4 className="text-3xl font-bold ">Wishlist ({listWishlist.length})</h4>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
-        {listWishlist.map((prd, i) => (
-          <Card prd={prd} key={i} />
-        ))}
-      </div>
-    </div>
+    <>
+      {" "}
+      {listWishlist.length > 0 ? (
+        <div>
+          <h4 className="text-3xl font-bold ">
+            Wishlist ({listWishlist.length})
+          </h4>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ">
+            {listWishlist.map((prd, i) => (
+              <Card prd={prd} key={i} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <p className="col-span-4 font-bold text-center text-3xl text-grey-500">
+          Your Wishlist is now empty.
+        </p>
+      )}
+    </>
   );
 }
